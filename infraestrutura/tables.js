2bc6d89b81b191cb3b23428a674018cec78b41e8
Ricardo Costa <ricardo_cas4@hotmail.com>
2021-09-11 17:57:54 -0300

    criado novos campos na tabela de appointments

--- infraestrutura/tables.js
+++ infraestrutura/tables.js
@@ -5,7 +5,7 @@ class Tables {
     }
 
     createAppointments() {
-        const sql = 'CREATE TABLE IF NOT EXISTS APPOINTMENTS (id int NOT NULL AUTO_INCREMENT, client varchar(50) NOT NULL, pet varchar(20), service varchar(20) NOT NULL, status_appointments varchar(20) NOT NULL, notes text, PRIMARY KEY(id))'
+        const sql = 'CREATE TABLE IF NOT EXISTS APPOINTMENTS (id int NOT NULL AUTO_INCREMENT, client varchar(50) NOT NULL, pet varchar(20), service varchar(20) NOT NULL, appointment_date datetime NOT NULL, created_at datetime NOT NULL, status_appointments varchar(20) NOT NULL, notes text, PRIMARY KEY(id))'
 
         this.connection.query(sql, (error) => {
             if (error) {
