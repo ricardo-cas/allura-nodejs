bbe84e48427e65e0c70d0da36cb8d7e78f4131b5
Ricardo Costa <ricardo_cas4@hotmail.com>
2021-09-11 19:02:35 -0300

    criado função para inserir os dados do atendimento via rota post.

--- models/appointments.js
+++ models/appointments.js
@@ -1,10 +1,14 @@
 const connection = require('./../infraestrutura/connection');
+const moment = require('moment');
 
 class Appoinment {
     addNewAppointment(appoinment) {
-        const sql = 'INSERT INTO appointments SET ?'
+        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS');
+        const data = moment(appoinment.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
+        const atendimentoDatado = { ...appoinment, dataCriacao, data }
+        const sql = 'INSERT INTO appointments SET ?';
 
-        connection.query(sql, appoinment, (error, result) => {
+        connection.query(sql, atendimentoDatado, (error, result) => {
             if (error) {
                 console.log(error);
             } else {
