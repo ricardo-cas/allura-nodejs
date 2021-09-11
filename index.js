16d2e4adf5673f76b0af5cd9ab7848e392676c09
Ricardo Costa <ricardo_cas4@hotmail.com>
2021-09-10 23:00:06 -0300

    criado rota principal

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ const app = express();
 app.listen(3000, () => {
     console.log(`Servidor ouvindo na porta 3000`);
 });
+
+app.get('/', (req, res) => {
+    res.send('Olá da rota principal 💯🆗💹');
+})
